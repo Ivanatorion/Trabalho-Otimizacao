9f56f14013d704b7d93f342ed40c1058d438b6e6
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-06-08 11:07:45 -0300

    Fix bug

--- analise/process_data.py
+++ analise/process_data.py
@@ -2,38 +2,46 @@ from pandas import DataFrame
 from glob import glob
 import os
 
+parameters = ['Esfriamento', 'nVizinhos', 'Temperatura']
+seeds = glob('Results/*')
 
-data = None
-results = [folder for folder in glob('Results/*/*/*') if os.path.isdir(folder)]
+for parameter in parameters:
+    data = None
 
-for result_folder in results:
-    params = {}
+    for seed_folder in seeds:
+        param_folder = os.path.join(seed_folder, parameter)
+        results = [folder for folder in glob(os.path.join(param_folder, '*')) if os.path.isdir(folder)]
 
-    with open(os.path.join(result_folder, 'Parametros.txt'), 'r') as params_file:
-        for line in params_file:
-            if len(line) > 1:
-                content = line.replace('\n', '').split(':')
-                key = content[0]
-                value = float(content[1].strip()) if key == 'Esfriamento' else int(content[1].strip())
-                params[key] = value
+        for result_folder in results:
+            params = {}
 
-    instances = [folder for folder in glob(os.path.join(result_folder, '*')) if os.path.isdir(folder)]
-    for instance_folder in instances:
-        instance_name = instance_folder.split('/')[-1]
-        params['Instancia'] = instance_name
+            with open(os.path.join(result_folder, 'Parametros.txt'), 'r') as params_file:
+                for line in params_file:
+                    if len(line) > 1:
+                        content = line.replace('\n', '').split(':')
+                        key = content[0]
+                        value = float(content[1].strip()) if key == 'Esfriamento' else int(content[1].strip())
+                        params[key] = value
 
-        with open(os.path.join(instance_folder, 'result.txt'), 'r') as result:
-            content = result.readlines()
+            instances = [folder for folder in glob(os.path.join(result_folder, '*')) if os.path.isdir(folder)]
+            for instance_folder in instances:
+                instance_name = instance_folder.split('/')[-1]
+                params['Instancia'] = instance_name
 
-            value_line = content[-8].replace('\n', '')
-            time_elapsed_line = content[-1].replace('\n', '')
+                with open(os.path.join(instance_folder, 'result.txt'), 'r') as result:
+                    content = result.readlines()
+                    content = ''.join(content)
 
-            params['Valor'] = int(value_line.strip().split(':')[-1])
-            params['Tempo'] = float(time_elapsed_line.strip().split(':')[-1])
+                    content = content.split('Valor: ')
+                    value_line = content[1].split('\n')[0]
+                    time_elapsed_line = content[1].split('Time elapsed (in minutes):')
 
-        if data is None:
-            data = DataFrame(data=params, index=[0])
-        else:
-            data = data.append(params, ignore_index=True)
+                    params['Valor'] = int(value_line)
+                    params['Tempo'] = float(time_elapsed_line[-1].strip().replace('\n', ''))
 
-data.to_csv('Data/data.csv', index=False)
+                if data is None:
+                    data = DataFrame(data=params, index=[0])
+                else:
+                    data = data.append(params, ignore_index=True)
+
+    data.to_csv('Data/{}.csv'.format(parameter), index=False)
